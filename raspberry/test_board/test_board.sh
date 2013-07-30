0a91715f805cf8432393a8e7540bd73c77832129
Charles-Henri Hallard <hallard04@free.fr>
2013-07-30 03:15:10 +0200

    Bug Fixes
    Set the serial to 9600 for test
    Flush the serial before test
    Grab the output of ArduiPi program to hex upcase

--- raspberry/test_board/test_board.sh
+++ raspberry/test_board/test_board.sh
@@ -2,25 +2,29 @@
 # quick and dirty code, I know, will arrange this later
 
 
-echo "Testing AruiPi Serial communication"
-echo "-----------------------------------"
+echo "Testing ArduiPi Serial communication"
+echo "------------------------------------"
 # configure serial port to Arduino compatible mode
-stty -F /dev/ttyAMA0 cs8 -cstopb -parenb 57600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts 
+stty -F /dev/ttyAMA0 cs8 -cstopb -parenb 9600 ignbrk -brkint -icrnl -imaxbel -opost -onlcr -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke noflsh -ixon -crtscts 
 echo -n "  Sending serial port command."
+echo "FLUSH" > /dev/ttyAMA0
+sleep 1
+
 # send I_AM_PI string
-echo "I_AM_PI" > /dev/ttyAMA0; 
+echo "I_AM_PI" > /dev/ttyAMA0
 echo -n "."
 # get I_AM_PI:OK response string from Arduino
-read RESPONSE < /dev/ttyAMA0 ; 
+read RESPONSE < /dev/ttyAMA0 
 echo -n "."
 if `echo ${RESPONSE} | grep "I_AM_PI:OK" 1>/dev/null 2>&1`
 then
 	# response OK send back ACK
   echo "Arduino Serial OK"
-	echo "ACK" > /dev/ttyAMA0; 
+	echo "ACK" > /dev/ttyAMA0
 else
   echo "Error No Serial response from Arduino"
 fi
+echo
 
 
 echo "Testing AruiPi I2C communication"
@@ -28,7 +32,7 @@ echo "-----------------------------------"
 
 echo -n "  Checking Arduino presence."
 # list I2C devices on bus and check Arduino is here, Arduino I2C address is 0x2a
-if `i2cdetect -y 1 | grep "2a" 1>/dev/null 2>&1`
+if `i2cdetect -y 1 | grep 2a 1>/dev/null 2>&1`
 then
   echo "Arduino I2C OK"
 else
@@ -38,12 +42,13 @@ fi
 echo -n "  Checking Arduino Ping."
 # send ping command to arduino, Arduino should respond 0x2a in ping response
 #if `i2cget -y 1 0x2a 0xe0 | grep "2a" 1>/dev/null 2>&1`
-if `arduipi --i2c --getbyte --hex --data 0xe0 | grep "0x2a" 1>/dev/null 2>&1`
+if `arduipi --i2c --getbyte --hex --data 0xe0 | grep 0x2A 1>/dev/null 2>&1`
 then
   echo "Arduino I2C Ping OK"
 else
   echo "Error in Ping response from Arduino"
 fi
+echo
 
 
 echo "Testing AruiPi SPI communication"
@@ -52,12 +57,13 @@ echo "-----------------------------------"
 echo -n "  Checking Arduino Ping."
 # send ping command to arduino, Arduino should respond 0x2a in ping response
 #if `i2cget -y 1 0x2a 0xe0 | grep "2a" 1>/dev/null 2>&1`
-if `arduipi --spi --getbyte --hex --data 0xe0 | grep "0x2a" 1>/dev/null 2>&1`
+if `arduipi --spi --ack --hex | grep 0x2A 1>/dev/null 2>&1`
 then
   echo "Arduino SPI Ping OK"
 else
   echo "Error in Ping response from Arduino"
 fi
+echo
 
 
 function setandclear
